b8b758236b0b7a9394cbc485732328827d5f0e0b
Adhil Salim <bizadhil@gmail.com>
2023-03-30 17:11:41 +0530

    Create navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+let links =[
+    {title: 'link-1', url: 'the link-1'},
+    {title: 'link-2', url: 'the link-2'},
+    {title: 'link-3', url: 'the link-3'},
+    ];
+    
+links.map((link)=>{
+        console.log(link.title, link.url);
+    });
